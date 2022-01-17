bc9f99f589570c31fdec5be95ad8b315103d1821
zaga1906 <edgar5271@hotmail.com>
2022-01-17 16:13:06 -0500

    Se crea documentacion con swagger gracias del API.

--- src/main/java/com/zaga/base/web/controller/ProductController.java
+++ src/main/java/com/zaga/base/web/controller/ProductController.java
@@ -2,6 +2,12 @@ package com.zaga.base.web.controller;
 
 import com.zaga.base.domain.Product;
 import com.zaga.base.domain.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,8 +28,13 @@ public class ProductController {
         return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
+    @Operation(summary = "Get a product")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the product", content = { @Content(mediaType = "application/json",  schema = @Schema(implementation = Product.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied", content = @Content),
+            @ApiResponse(responseCode = "404", description = "product not found", content = @Content) })
     @GetMapping("/{productId}")
-    public ResponseEntity<Product> getProduct(@PathVariable("productId") int productId) {
+    public ResponseEntity<Product> getProduct(@Parameter(description = "Id of the product", required = true, example = "5") @PathVariable("productId") int productId) {
         return productService.getProduct(productId)
                 .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
         .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
